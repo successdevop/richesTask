963b022da756e12bfd8bda86f66000e9942237f0
successdevop <successraphael28@gmail.com>
2024-02-21 15:07:32 +0100

    successfully implemented the getSingleBook and deleteSingleBook controller

--- CRUD/controllers/booksControllers.js
+++ CRUD/controllers/booksControllers.js
@@ -5,7 +5,7 @@ const { customError } = require("../error/errorClass");
 const getAllBooks = asyncWrapper((req, res, next) => {
   fs.readFile("./database.json", "utf8", (err, json) => {
     if (err) {
-      next(customError(err.message, 400));
+      next(customError(err, 400));
       return;
     }
 
@@ -24,9 +24,7 @@ const getAllBooks = asyncWrapper((req, res, next) => {
 const createABook = asyncWrapper((req, res, next) => {
   const { bookName, bookAuthor } = req.body;
   if (!bookName || !bookAuthor) {
-    next(
-      customError("book name/title and author's name must be provided", 400)
-    );
+    next(customError("book title and author's name must be provided", 400));
     return;
   }
 
@@ -43,31 +41,119 @@ const createABook = asyncWrapper((req, res, next) => {
 
   fs.readFile("./database.json", "utf8", (err, data) => {
     if (err) {
-      next(customError(err.message, 400));
+      next(customError(err, 400));
       return;
     }
 
     const dataDB = [...JSON.parse(data), newData];
     fs.writeFile("./database.json", JSON.stringify(dataDB), (err) => {
       if (err) {
-        next("Error writing file", 404);
+        next(customError("Error writing file", 400));
         return;
       }
 
-      res
-        .status(201)
-        .json({ status: "success", msg: "Book added successfully" });
+      res.status(201).json({
+        status: "success",
+        msg: "Book added successfully",
+        data: newData,
+      });
     });
   });
 });
 
-const getSingleBook = asyncWrapper((req, res, next) => {});
+const getSingleBook = asyncWrapper((req, res, next) => {
+  const { id } = req.params;
+  fs.readFile("./database.json", "utf8", (err, data) => {
+    if (err) {
+      next(err, 400);
+      return;
+    }
 
-const updateABook = asyncWrapper((req, res, next) => {});
+    const book = JSON.parse(data).find((bk) => bk.id == id);
 
-const deleteABook = asyncWrapper((req, res, next) => {});
+    if (!book) {
+      next(customError(`Book with id (${id}) does not exist!!!`, 400));
+      return;
+    }
 
-const deleteAllBooks = asyncWrapper((req, res, next) => {});
+    res.status(200).json({ status: "success", book });
+  });
+});
+
+const updateABook = asyncWrapper((req, res, next) => {
+  const { id } = req.params;
+  const { bookName, bookAuthor } = req.body;
+  if (!bookName || !bookAuthor) {
+    next(customError("book title and author's name must be provided", 400));
+    return;
+  }
+
+  fs.readFile("./database.json", "utf8", (err, data) => {
+    if (err) {
+      next(customError(err, 400));
+      return;
+    }
+    const book = JSON.parse(data).find((bk) => bk.id === id);
+    if (!book) {
+      next(customError(`Book with id (${id}) does not exist!!!`, 400));
+      return;
+    }
+    const { bookName, bookAuthor } = book;
+    console.log(bookName);
+    const dataDB = [...JSON.parse(data), book];
+    fs.writeFile("./database.json", JSON.stringify(dataDB), (err) => {
+      if (err) {
+        next(customError("Error writing file", 400));
+        return;
+      }
+
+      res.status(200).json({
+        status: "success",
+        msg: "Book updated successfully",
+        data: newData,
+      });
+    });
+  });
+});
+
+const deleteABook = asyncWrapper((req, res, next) => {
+  const { id } = req.params;
+  fs.readFile("./database.json", "utf8", (err, data) => {
+    if (err) {
+      next(customError(err, 400));
+      return;
+    }
+    // check for the presence of book
+    const book = JSON.parse(data).find((bk) => bk.id === id);
+    if (!book) {
+      next(customError(`Book with id (${id}) does not exist!!!`, 400));
+      return;
+    }
+    // delete a single book if present
+    const newData = JSON.parse(data).filter((bks) => bks.id !== id);
+    // write back our available book to database(json)
+    fs.writeFile("./database.json", JSON.stringify(newData), (err) => {
+      if (err) {
+        next(customError("Error writing file", 400));
+        return;
+      }
+
+      res.status(200).json({
+        status: "success",
+        msg: `Book with id (${id}) deleted successfully`,
+      });
+    });
+  });
+});
+
+const deleteAllBooks = asyncWrapper((req, res, next) => {
+  fs.unlink("./database.json", (err) => {
+    if (err) {
+      next(customError("could not delete file", 500));
+    }
+    res.status(200).json({ status: "success", msg: "File deleted" });
+  });
+});
 
 module.exports = {
   getAllBooks,
